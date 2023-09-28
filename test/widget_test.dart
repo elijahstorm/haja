3c2bdb38796ecbd1c582f3619d4c5651faafdc87
elijahstorm <95153181+elijahstorm@users.noreply.github.com>
2023-09-28 11:47:00 +0700

    add testing

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,22 +8,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:proj_refresh/main.dart';
+import 'package:haja/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
+    await tester.pumpWidget(const HajaDoTogetherApp());
+    expect(find.text('Haja'), findsOneWidget);
     await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
-
-    // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
   });
